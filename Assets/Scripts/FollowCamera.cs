feb918bf166ea705dafc5c5fdeb9eff447ce8d48
alexis-valois <alexis.valois@hotmail.com>
2016-12-03 22:11:22 -0500

    debut gestion follow camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    /*
+     * Code source inspiré de :
+     * https://code.tutsplus.com/tutorials/unity3d-third-person-cameras--mobile-11230
+     */
+
+    public GameObject target;
+    public float damping = 1;
+    Vector3 offset;
+
+    // Use this for initialization
+    void Start () {
+        offset = target.transform.position - transform.position;
+	}
+
+	void LateUpdate () {
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = target.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        //transform.position = target.transform.position - (rotation * offset);
+
+        transform.LookAt(target.transform);
+    }
+}
--- Assets/Scripts/Rej.cs
+++ Assets/Scripts/Rej.cs
@@ -2,34 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 /*
- * Code source inspiré de  : https://unity3d.com/fr/learn/tutorials/topics/scripting/translate-and-rotate?playlist=17117
+ * Code source inspiré de  : 
+ * https://unity3d.com/fr/learn/tutorials/topics/scripting/translate-and-rotate?playlist=17117
+ * http://answers.unity3d.com/questions/9246/playing-an-animation-while-moving-a-character-forw.html
  */
 public class Rej : MonoBehaviour {
 
     // Public variables that will show up in the Editor
-    public float moveSpeed = 10f;
-    public float turnSpeed = 50f;
-
-    private Rigidbody rbody;
+    public float maxWalkSpeed = 60f;
+    public float maxTurnSpeed = 80f;
+    Animator anim;
 
     void Start()
     {
-        rbody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        var walkSpeed = Input.GetAxis("Vertical") * maxWalkSpeed;
+        var turnSpeed = Input.GetAxis("Horizontal") * maxTurnSpeed;
 
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        transform.Translate(-Vector3.forward * walkSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        if (walkSpeed > 5)
+        {
+            anim.Play("Walk");
+        }else
+        {
+            anim.Play("Idle");
+        }
     }
-
 }
