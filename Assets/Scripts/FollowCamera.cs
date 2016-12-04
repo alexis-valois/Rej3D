bea6734c43fcef3cfb3cd34d0897b04ec843aaa1
Alexis Valois-Adamowicz <valois-adamowicz.alexis@courrier.uqam.ca>
2016-12-04 12:11:56 -0500

    début gestion des jumps

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -5,27 +5,55 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour {
 
     /*
-     * Code source inspiré de :
+     * Code source inspiré du fichier SmoothFollow.cs inclus dans les Standard Assets de Unity ainsi que :
      * https://code.tutsplus.com/tutorials/unity3d-third-person-cameras--mobile-11230
      */
 
-    public GameObject target;
-    public float damping = 1;
-    Vector3 offset;
+    // The target we are following
+    [SerializeField]
+    private Transform target;
+    // The distance in the x-z plane to the target
+    [SerializeField]
+    private float distance = 10.0f;
+    // the height we want the camera to be above the target
+    [SerializeField]
+    private float height = 5.0f;
+    [SerializeField]
+    private float rotationDamping = 5f;
+    [SerializeField]
+    private float heightDamping = 5f;
 
     // Use this for initialization
-    void Start () {
-        offset = target.transform.position - transform.position;
-	}
+    void Start () {	}
 
 	void LateUpdate () {
-        float currentAngle = transform.eulerAngles.y;
-        float desiredAngle = target.transform.eulerAngles.y;
-        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+        if (!target)
+            return;
+        // Calculate the current rotation angles
+        var wantedRotationAngle = target.eulerAngles.y;
+        var wantedHeight = target.position.y + height;
 
-        Quaternion rotation = Quaternion.Euler(0, angle, 0);
-        //transform.position = target.transform.position - (rotation * offset);
+        var currentRotationAngle = transform.eulerAngles.y;
+        var currentHeight = transform.position.y;
 
-        transform.LookAt(target.transform);
+        // Damp the rotation around the y-axis
+        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+
+        // Damp the height
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        // Convert the angle into a rotation
+        var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+        // Set the position of the camera on the x-z plane to:
+        // distance meters behind the target
+        transform.position = target.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+
+        // Set the height of the camera
+        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+        // Always look at the target
+        transform.LookAt(target);
     }
 }
--- Assets/Scripts/Rej.cs
+++ Assets/Scripts/Rej.cs
@@ -5,17 +5,21 @@ using UnityEngine;
  * Code source inspiré de  : 
  * https://unity3d.com/fr/learn/tutorials/topics/scripting/translate-and-rotate?playlist=17117
  * http://answers.unity3d.com/questions/9246/playing-an-animation-while-moving-a-character-forw.html
+ * http://answers.unity3d.com/questions/196381/how-do-i-check-if-my-rigidbody-player-is-grounded.html
  */
 public class Rej : MonoBehaviour {
 
     // Public variables that will show up in the Editor
     public float maxWalkSpeed = 60f;
     public float maxTurnSpeed = 80f;
+    public float jumpForce = 5f;
     Animator anim;
+    Rigidbody rBody;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        rBody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
@@ -23,12 +27,19 @@ public class Rej : MonoBehaviour {
         var walkSpeed = Input.GetAxis("Vertical") * maxWalkSpeed;
         var turnSpeed = Input.GetAxis("Horizontal") * maxTurnSpeed;
 
-        transform.Translate(-Vector3.forward * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
         transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.Play("Jump");
+            rBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
 
         if (walkSpeed > 5)
         {
             anim.Play("Walk");
+            anim.speed = 2;
         }else
         {
             anim.Play("Idle");
